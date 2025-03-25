63ff90f9c2e8d2e08ec00ce1f7e31d6df2650703
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-03-25 08:19:17 +0530

    - Rename 'liv' parameter to 'lives' - Remove redundant loops in filter_dictionary - Remove 'final_words' attribute - Optimize filtering logic in two stages:   1. Position matching for known letters   2. Letter frequency matching for guessed/given letters

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ from english_words import get_english_words_set
 
 
 class Hangman:
-    def __init__(self, word, liv=6):
+    def __init__(self, word, lives=6):
         self.word = word.lower()
         self.word_length = len(self.word)
-        self.remaining_lives = liv
+        self.remaining_lives = lives
         self.game_over = False
         self.remaining_words = set()
-        self.final_words = set()
         self.given_indices = [i for i in range(self.word_length) if self.word[i] != "_"]
-        self.guessed_letters = [self.word[index] for index in self.given_indices]
+        self.guessed_letters = {self.word[index] for index in self.given_indices}
 
     def start_game(self):
         self.filter_dictionary(self.word_length)
 
     def filter_dictionary(self, word_length):
-        dict_words = list(get_english_words_set(["web2"], lower=True))
+        dict_words = get_english_words_set(["web2"], lower=True)
         filtered_words = {word for word in dict_words if len(word) == word_length}
 
         for dict_word in filtered_words:
@@ -28,20 +27,16 @@ class Hangman:
             if match:
                 self.remaining_words.add(dict_word)
 
-        for dict_word in self.remaining_words:
-            match = True
-            for i in range(word_length):
-                if self.word[i] == "_":
-                    continue
-                if dict_word[i] != self.word[i]:
-                    match = False
-                    break
-            if match:
-                self.final_words.add(dict_word)
+        filtered_words = self.remaining_words.copy()
+
+        for letter in self.guessed_letters:
+            for word in filtered_words:
+                if word.count(letter) != self.word.count(letter):
+                    self.remaining_words.remove(word)
 
 
 if __name__ == "__main__":
-    game = Hangman("a_pl_")
+    game = Hangman("s_mm_tr_")
     game.start_game()
     print(game.guessed_letters)
-    print(game.final_words)
+    print(game.remaining_words)
